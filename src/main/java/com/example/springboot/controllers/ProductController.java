5ac2b5a01410e401f9e80d0722121aeaa7418a3b
ViniciusFariasSantos <spviniciusfariassantos@gmail.com>
2024-07-25 00:36:46 -0300

    add config put and delete

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -42,5 +42,34 @@ public class ProductController {
         }
         return ResponseEntity.status(HttpStatus.OK).body(productO.get());
     }
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object>
+    deleteProduct(@PathVariable(value="id") UUID id) {
+        Optional<ProductModel> productO =
+                productRepository.findById(id);
+        if(productO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Product not found.");
+        }
+        productRepository.delete(productO.get());
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("Product deleted successfully.");
+    }
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Object>
+    updateProduct(@PathVariable(value="id") UUID id,
+                  @RequestBody @Valid ProductRecordDto productRecordDto) {
+        Optional<ProductModel> productO =
+                productRepository.findById(id);
+        if(productO.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Product not found.");
+        }
+        var productModel = productO.get();
+        BeanUtils.copyProperties(productRecordDto,
+                productModel);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(productRepository.save(productModel));
+    }
 
 }
